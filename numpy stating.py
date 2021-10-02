7a3b71cde93bf86b42394e27c690ba89abf3c28a
Adarsh Sharma <83903199+Adarsh857@users.noreply.github.com>
2021-10-03 05:23:59 +0530

    Add files via upload

---
+++ numpy stating.py
@@ -0,0 +1,15 @@
+'''import numpy as np
+a=np.array([1,2,3,4])
+b=np.array([9,8,7,6])
+import matplotlib.pyplot as plt
+plt.plot(a,b)
+plt.show()'''
+import numpy as np
+a=np.array([1,2,3,4])
+b=np.array([9,8,7,6])
+import matplotlib.pyplot as plt
+plt.xlabel('this is the x axis')
+plt.ylabel('this is the y axis')
+plt.title('this is a title')
+plt.plot(a,b)
+plt.show()
---
+++ oops.py
@@ -0,0 +1,38 @@
+# class :it is a blueprint for the set of staes and behaviour
+#object: smallest entity of the class which has all the states amd behaviour o fthe class
+#class ClassName():
+#function/data
+#object_name=ClassName()
+'''class Student():
+    number1=10
+    def marks(self):
+        print("you passs")
+
+obj=Student()
+obj.marks()
+print(obj.number1)'''
+
+#inheritance: when the property of one class is used in another class it is known as inheritence
+#there are total of 5 tyoes of inheritence
+#single: t child 1 parent
+'''class Parent():
+    n=10
+class Child(Parent):
+    m=20
+    obj=Child()
+    print(obj.n)'''
+#hierarchical - 1 parent mutiple child
+#multilevel-inheritene in diff levels in program
+'''class Parent():
+    n=10
+class Child(Parent):
+    m=20
+    class Child2(Child):
+        o=9
+        obj=Child2()
+    obj=Child()
+    print(obj.n)
+# multiple:more then one parent and a child'''
+#hybrid:combinaton of above four
+
+#constructor:special function
